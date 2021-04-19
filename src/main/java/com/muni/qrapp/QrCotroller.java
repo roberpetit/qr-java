87dad76a45a5bf44136c7580db4a4bb9beacbaab
Roberto Petit de Meurville <roberto.petit-de-meurville@ext.prosegur.com>
2021-04-18 22:11:13 -0300

    find all - change pswd db

--- src/main/java/com/muni/qrapp/QrCotroller.java
+++ src/main/java/com/muni/qrapp/QrCotroller.java
@@ -3,6 +3,7 @@ package com.muni.qrapp;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @CrossOrigin
@@ -19,6 +20,12 @@ public class QrCotroller {
     public QrEntity findById(@PathVariable String id) {
         System.out.println("==== get qr ====");
         return restRepository.findById(UUID.fromString(id));
+    }
+
+    @GetMapping("/findall")
+    public List<QrEntity> findAll() {
+        System.out.println("==== get all qrs ====");
+        return restRepository.findAll();
     }
 
     @PostMapping(path = "/save", consumes = "application/json", produces = "application/json")
--- src/main/java/com/muni/qrapp/QrRestRepository.java
+++ src/main/java/com/muni/qrapp/QrRestRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
@@ -11,4 +12,5 @@ public interface QrRestRepository extends JpaRepository<QrEntity, String> {
 
     QrEntity findById(@Param("id") UUID id);
     QrEntity save(QrEntity qr);
+    List<QrEntity> findAll();
 }
